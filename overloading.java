218f9556a637b093c8263da88f2f3563a71a7214
MAKMOT JOHNSON <makmotjohnson7@gmail.com>
2024-04-10 00:00:02 +0300

    Add files via upload

---
+++ overloading.java
@@ -0,0 +1,12 @@
+public class overloading{
+    static int plusMethod(int x, int y){
+        return x+y;
+    }
+
+    static double plusMethod(double x, double y){
+        return x+y;
+    }
+    public static void main(String[]args){
+        int myNumber1 = plusMethod
+    }
+}
